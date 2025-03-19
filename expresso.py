1165363152080aedf7d8bece35bd8bfcc94f2742
msane10 <53219252+msane10@users.noreply.github.com>
2025-03-19 21:52:27 +0000

    Update expresso.py

--- expresso.py
+++ expresso.py
@@ -14,7 +14,7 @@ st.subheader("Auteur: Professeur M. Diop")
 
     # Définition de la fonction pour télécharger les données
 
-data = pd.read_csv("Expresso_churn_dataset.csv")
+data = pd.read_csv("https://drive.google.com/file/d/1rIOP3BehV-vw-NijVmqTAc9ZD1PBeIjw/view?usp=sharing")
 
 
 # Afficher des informations générales sur l'ensemble de données
